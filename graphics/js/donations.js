c7e4df28fe177ee325d203c5790d92c7acc280c6
DorkmasterFlek <dorkmasterflek@gmail.com>
2018-11-28 13:35:32 -0500

    Make donation total animate up to the new number

--- graphics/js/donations.js
+++ graphics/js/donations.js
@@ -3,9 +3,22 @@ $(() => {
     // JQuery selectors.
     let donationTotalElement = $('#donationTotal');
 
-    // Update donation total when changed.
+    // Update donation total when changed.  Animate the number increasing to the new total.
     let donationTotal = nodecg.Replicant('donationTotal');
     donationTotal.on('change', (newVal, oldVal) => {
-        donationTotalElement.html(currencyFormatter.format(newVal));
+        $({countNum: oldVal}).animate({
+                countNum: newVal,
+            },
+            {
+                duration: 1000,
+                easing: 'swing',
+                step: function() {
+                    donationTotalElement.text(currencyFormatter.format(this.countNum));
+                },
+                complete: function() {
+                    donationTotalElement.text(currencyFormatter.format(this.countNum));
+                },
+            }
+        );
     });
 });
