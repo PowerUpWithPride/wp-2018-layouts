518b60d7a7cc9939a6e739fb4d094d09ac669e0d
DorkmasterFlek <dorkmasterflek@gmail.com>
2018-11-30 15:17:57 -0500

    Display whole dollar amounts only

--- graphics/js/utils.js
+++ graphics/js/utils.js
@@ -2,6 +2,25 @@
 const currencyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0
 });
+
+// Converts milliseconds to a time string.
+function msToTime(duration, noHour) {
+    let seconds = parseInt((duration / 1000) % 60),
+        minutes = parseInt((duration / (1000 * 60)) % 60),
+        hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    let timeString = '';
+
+    if (!noHour)
+        timeString += hours+':';
+    timeString += minutes + ':' + seconds;
+
+    return timeString;
+}
